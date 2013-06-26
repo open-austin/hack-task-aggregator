c9fd0e210c5f58f306b970f93095d21820d5b4dd
Chip Rosenthal <chip@unicom.com>
2013-06-26 02:39:19 -0500

    Added api docs. Added optional app settings to QueryProjectGitHub.

--- public/assets/hack-task-aggregator.js
+++ public/assets/hack-task-aggregator.js
@@ -1,13 +1,45 @@
+/**
+ * Display an alert to the user.
+ * 
+ * @param mssg  Message text.
+ * @return  nothing
+ * 
+ * This simply calls the Javascript alert() function. It
+ * can be overridden later, once the framework is running,
+ * to display the alerts as message boxes on the page.
+ */
 var appAlert = function(mssg) {
   alert(mssg);
 }
 
+/**
+ * Terminate a thread with an error.
+ * 
+ * @param mssg  Error messsage text.
+ * @param trace  Object with trace information.
+ * @return  does not return
+ * 
+ * The message will be displayed with appAlert().
+ * The message and trace information will be logged
+ * to the console as an error.
+ * Finally, an exception will be thrown.
+ */
 var error = function(mssg, trace) {
   appAlert(mssg);
   console.error({'message': mssg, 'trace': trace});
   throw new Error(mssg);
 }
 
+
+/**
+ * Handle failure of a jQuery AJAX operation.
+ * 
+ * Invokes error() to handle an AJAX operation failure.
+ * 
+ * Example usage is:
+ * 
+ *   $.getJSON() ... .fail(ajaxFailHandler) ...;
+ */
 function ajaxFailHandler(jqxhr, textStatus, err) {
   error("Request failed: " + textStatus + ', ' + err,
     {'jqxhr' : jqxhr, 'textStatus' : textStatus, 'error' : err});
@@ -36,21 +68,47 @@ function getQueryParams(url) {
   return params;
 }
 
+/**
+ * Object containing the current query parameters.
+ */
 var QUERY_PARAMS = getQueryParams(document.URL);
 
 
-var HackTaskAggregator = function(projectDefs) {
+/**
+ * Class that implements that Hack Task Aggregator application.
+ * 
+ * @param projectDefs  List of project definitions.
+ * @param appDefs  Parameter settings for the application.
+ * 
+ * TODO - Add description for the parameters. 
+ */
+var HackTaskAggregator = function(projectDefs, appDefs) {
+  
+  if (! appDefs) {
+    appDefs = {};
+  }  
+  appDefs.DEFAULT_LABEL = appDefs.DEFAULT_LABEL || "hack";
+  appDefs.ALL_LABEL = appDefs.ALL_LABEL || "*";
   
   var self = this;
   
   self.view = new ViewModel();
   ko.applyBindings(self.view);
   
-  /* change appAlert() to display alert on page rather than a pop-up */
+  /*
+   * Now that the framework is running, override appAlert()
+   * to display alert on page rather than a Javascript
+   * alert() pop-up.
+   */
   appAlert = self.view.alert;
   
+  /* 
+   * Process each of the project definitions,
+   * query the repository for hack tasks,
+   * and add them too the view instance.
+   */
   projectDefs.forEach(function(projectDef) {
-    new QueryProjectGitHub(projectDef)
+    new QueryProjectGitHub(projectDef, appDefs)
     .query(function(projectInfo) {
       self.view.projects.push(projectInfo);      
     });
@@ -59,30 +117,44 @@ var HackTaskAggregator = function(projectDefs) {
 };
 
 
-var QueryProjectGitHub = function(repo) {
-  
-  var DEFAULT_LABEL = "hack";
-  var ALL_LABEL = "*";
+/**
+ * Class that implements the project query operations for GitHub.
+ * 
+ * @param projectDef
+ * @param appDefs
+ * 
+ * TODO - Add description for the parameters. 
+ * 
+ * At some point we may want to abstract this, so there can
+ * be a variety of classes that handle different task repositories.
+ */
+var QueryProjectGitHub = function(projectDef, appDefs) {
   
   var self = this;
   
-  if (! repo.owner) {
-    error("Error in repository definition: required parameter \"owner\" not defined", {'repo' : repo});
+  if (! projectDef.owner) {
+    error("Error in repository definition: required parameter \"owner\" not defined", {'projectDef' : projectDef});
   }
-  self.owner = repo.owner;
+  self.owner = projectDef.owner;
   
-  if (! repo.project) {
-    error("Error in repository defintion: required parameter \"project\" not defined", {'repo' : repo});
+  if (! projectDef.project) {
+    error("Error in repository defintion: required parameter \"project\" not defined", {'projectDef' : projectDef});
   }
-  self.project = repo.project;
+  self.project = projectDef.project;
   
-  self.label = repo.label || DEFAULT_LABEL;
-  if (self.label == ALL_LABEL) {
+  self.label = projectDef.label || appDefs.DEFAULT_LABEL;
+  if (self.label == appDefs.ALL_LABEL) {
     self.label = undefined;
   }
   
   self.baseURL = "https://api.github.com/repos/" + encodeURIComponent(self.owner) + "/" + encodeURIComponent(self.project);
   
+  /**
+   * Build a URL to query a GitHub repo.
+   * 
+   * @param path  Path to append to baseURL, if any. Must start with "/".
+   * @param params  Optional object of query parameter values, keyed by parameter name.
+   */
   self.buildURL = function(path, params) {
     if (! params) {
       params = {};
@@ -99,6 +171,15 @@ var QueryProjectGitHub = function(repo) {
     return self.baseURL + (path ? path : "") + "?callback=?&" + $.param(params);
   };  
 
+  /**
+   * Check the "meta.status" value of a GitHub query.
+   * 
+   * @param response The query response
+   * @param context The calling context, e.g. "this" in the caller.
+   * @return nothing
+   * 
+   * Calls error() if the query status indicates the request failed.
+   */
   self.checkResponse = function(response, context) {
     if (response.meta.status != 200) {
       error("An error occurred while trying to retrieve information on project "
@@ -110,6 +191,9 @@ var QueryProjectGitHub = function(repo) {
     }
   };
   
+  /**
+   * TODO - document me
+   */
   self.query = function(callback) {
     $.getJSON(self.buildURL())
     .done(function(response) {
@@ -118,7 +202,10 @@ var QueryProjectGitHub = function(repo) {
     })
     .fail(ajaxFailHandler);
   };
-  
+
+  /**
+   * TODO - document me
+   */
   self.queryTasks = function(project, callback) {
     var params = {};
     if (self.label) {
@@ -143,12 +230,18 @@ var QueryProjectGitHub = function(repo) {
 };
 
 
+/**
+ * TODO - document me
+ */
 var ViewModel = function() {
   var self = this;
   
   self.projects = ko.observableArray();
   self.alerts = ko.observableArray();
 
+  /**
+   * TODO - document me
+   */
   self.alert = function(message, options) {
     if (! options) {
       options = {};
